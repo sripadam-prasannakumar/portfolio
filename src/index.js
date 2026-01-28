00b9bf6e4984d74dd7f9e4f1e0d8f029f3c87304
Prasannakumar <prasannakumar@gmail.com>
2026-01-28 16:37:51 +0530

    Remove basename for Netlify routing

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import App from "./App";
+
+// const root = ReactDOM.createRoot(
+//   document.getElementById("root")
+// );
+
+// const basename =
+//   process.env.NODE_ENV === "production"
+//     ? "/"
+//     : "/";
+
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter basename={basename}>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -7,14 +30,9 @@ const root = ReactDOM.createRoot(
   document.getElementById("root")
 );
 
-const basename =
-  process.env.NODE_ENV === "production"
-    ? "/"
-    : "/";
-
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
